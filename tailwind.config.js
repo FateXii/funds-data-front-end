19b6b0e01a3573dbca9153a3e11a5049435947dc
FateXii <rambane.t.r@gmail.com>
2021-01-10 18:09:46 +0200

    final fix for purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
-  purge: ['./src/**/*.html',
-    './src/**/*.js'],
+  purge: {
+    content: ['./public/**/*.html', './src/**/*.vue'],
+    options: {
+      whitelistPatterns: [
+        /-(leave|enter|appear)(|-(to|from|active))$/,
+        /^(?!(|.*?:)cursor-move).+-move$/,
+        /^router-link(|-exact)-active$/,
+      ],
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
