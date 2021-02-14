ad569e2cafb64dba163ad1335f43759fbd9ca48a
Thendo Rambane <rambane.t.r@gmail.com>
2021-02-14 11:33:44 +0200

    Add AuthForm component

---
+++ src/components/AuthForm.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="form-contaniner">
+    <el-form>
+      <el-form-item label="Name">
+        <el-input></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
