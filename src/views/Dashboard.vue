b5044cb1e04cd47e4475493794aa98939e0f38cb
Thendo Rambane <rambane.t.r@gmail.com>
2021-02-26 19:53:49 +0200

    Revert "Pull out goal cards"
    This reverts commit 67057e50cafcbc015c89e0677cd5b1c9202b21e6.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,82 +1,67 @@
 <template>
-  <div class="goal-cards">
-    <el-card
-      v-for="goal in goals"
-      :key="goal.id"
+  <el-container>
+    <div class="goal-cards">
+      <template
+        v-for="goal in goals"
+        :key="goal.id"
+      >
+        <InvestmentCard :goal="goal" />
+      </template>
+    </div>
+    <el-skeleton
       class="box-card"
+      v-if="goalsLength === 0"
     >
-      <template #header>
-        <div class="card-header">
-          <h4>{{goal.name}}</h4>-
-          <h4>Goal Amount</h4>
-          <p>R {{goal.investmentGoal}}</p>
-          <p v-if="goal.description">{{goal.description}}</p>
+      <template #template>
+        <el-skeleton-item
+          variant="image"
+          style="width: 240px; height: 240px;"
+        />
+        <div style="padding: 14px;">
+          <el-skeleton-item
+            variant="p"
+            style="width: 50%"
+          />
+          <div
+            style="display: flex; align-items: center; justify-items: space-between;"
+          >
+            <el-skeleton-item
+              variant="text"
+              style="margin-right: 16px;"
+            />
+            <el-skeleton-item
+              variant="text"
+              style="width: 30%;"
+            />
+          </div>
         </div>
       </template>
-      <div>
-        <h5>Investing</h5>
-        <span
-          v-if="goal.investmentAmount.principle"
-        >R{{goal.investmentAmount.principle}} Ininital investment</span>
-        <br />
-        <span
-          v-if="goal.investmentAmount.installment"
-        >R{{goal.investmentAmount.installment}} p/m</span>
-
-        <h5>Investing for:</h5>
-        <p>{{goal.length}} years</p>
-      </div>
-    </el-card>
-  </div>
-  <el-skeleton
-    class="box-card"
-    v-if="goalsLength === 0"
-  >
-    <template #template>
-      <el-skeleton-item
-        variant="image"
-        style="width: 240px; height: 240px;"
-      />
-      <div style="padding: 14px;">
-        <el-skeleton-item
-          variant="p"
-          style="width: 50%"
-        />
-        <div
-          style="display: flex; align-items: center; justify-items: space-between;"
-        >
-          <el-skeleton-item
-            variant="text"
-            style="margin-right: 16px;"
-          />
-          <el-skeleton-item
-            variant="text"
-            style="width: 30%;"
-          />
-        </div>
-      </div>
-    </template>
-  </el-skeleton>
-  <div>
-    <el-button
-      type="primary"
-      @click="goToAddGoals"
-    >Add More Goals</el-button>
-  </div>
+    </el-skeleton>
+    <div>
+      <el-button
+        type="primary"
+        @click="goToAddGoals"
+      >Add More Goals</el-button>
+    </div>
+  </el-container>
 </template>
 
 <script lang="ts">
 import { computed, onMounted, watch } from 'vue';
 import { useStore } from 'vuex';
 import { useRouter } from 'vue-router';
+import InvestmentCard from '@/components/InvestmentCard.vue';
 
 export default {
+  components: {
+    InvestmentCard,
+  },
   setup() {
     const store = useStore();
     const router = useRouter();
     const loggedIn = computed(() => store.state.user.loggedIn);
-    watch(loggedIn, () => {
-      if (!loggedIn.value) {
+    watch(loggedIn, (newValue) => {
+      if (!newValue) {
         router.push('authentication');
       }
     });
@@ -93,6 +78,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.el-container {
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+}
 .goal-cards {
   display: flex;
   position: relative;
